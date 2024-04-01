5ff155bcef7d2f3d9a2bb1866e7893c848db4e8c
devzyh <devzyh@outlook.com>
2024-04-01 18:09:32 +0800

    Bean容器

---
+++ mini-spring/src/main/java/cn/devzyh/minispring/beans/factory/config/BeanDefinition.java
@@ -0,0 +1,22 @@
+package cn.devzyh.minispring.beans.factory.config;
+
+/**
+ * Bean对象定义
+ */
+public class BeanDefinition {
+
+    private Class beanClass;
+
+    public BeanDefinition(Class beanClass) {
+        this.beanClass = beanClass;
+    }
+
+    public Class getBeanClass() {
+        return beanClass;
+    }
+
+    public void setBeanClass(Class beanClass) {
+        this.beanClass = beanClass;
+    }
+
+}
---
+++ mini-spring/src/main/java/cn/devzyh/minispring/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -0,0 +1,30 @@
+package cn.devzyh.minispring.beans.factory.support;
+
+import cn.devzyh.minispring.beans.factory.config.SingletonBeanRegistry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 默认实现类
+ */
+public class DefaultSingletonBeanRegistry implements SingletonBeanRegistry {
+
+    private Map<String, Object> singletons = new HashMap<>();
+
+    @Override
+    public Object getSingleton(String beanName) {
+        return singletons.get(beanName);
+    }
+
+    /**
+     * 添加单例对象
+     *
+     * @param beanName
+     * @param obj
+     */
+    protected void addSingleton(String beanName, Object obj) {
+        this.singletons.put(beanName, obj);
+    }
+
+}
